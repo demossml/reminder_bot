e84b6032409640022226042038ff30aa5d6110aa
demossml <72225698+demossml@users.noreply.github.com>
2024-07-27 10:42:55 +0300

    Update server_init.py

--- server_init.py
+++ server_init.py
@@ -1,9 +1,3 @@
 from subprocess import Popen
 import asyncio
 
-filename = "run python3 bot/__init__.py"
-
-while True:
-    print("\nStarting " + filename)
-    p = Popen("/Library/Frameworks/Python.framework/Versions/3.9/bin/poetry " + filename, shell=True)
-    p.wait()
